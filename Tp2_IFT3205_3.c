953397362bc8c251e0fe54984e1b0eb060b11971
Therk <43972622+Therrk@users.noreply.github.com>
2023-02-19 13:36:24 -0500

    question 3 et 4!

--- Tp2_IFT3205_3.c
+++ Tp2_IFT3205_3.c
@@ -23,7 +23,7 @@
 #define NAME_VISUALISER "display "
 #define NAME_IMG_IN1  "UdM_1"
 #define NAME_IMG_IN2  "UdM_2"
-#define NAME_IMG_OUT1 "lena_rotated"
+#define NAME_IMG_OUT1 "image-Out1"
 #define NAME_IMG_OUT2 "image-Out2"
 
 /*------------------------------------------------*/
@@ -43,10 +43,32 @@ int main(int argc,char **argv)
     //Constante
     length=512;
     width=512;
+    int mask_size= 50;
 
-    float** udem1_r = LoadImagePgm(NAME_IMG_IN1, &length, &width);
+    float** udem1 = LoadImagePgm(NAME_IMG_IN1, &length, &width);
+    float** udem1_backup = fmatrix_allocate_2d(length, width);
+    float** udem1_r = fmatrix_allocate_2d(length, width);
     float** udem1_i = fmatrix_allocate_2d(length, width);
+    fmatrix_move(length, width, udem1, udem1_backup);
+
+    float** udem2 = LoadImagePgm(NAME_IMG_IN2, &length, &width);
+    float** udem2_backup = fmatrix_allocate_2d(length, width);
+    fmatrix_move(length, width, udem2, udem2_backup);
+
+    // les rebords noirs de l'image tournée cause des difficultés lorsqu'on
+    // trouve best_theta, on ajoute donc un cadre noir pour rendre les rebords
+    // des deux images pareilles
+    for (int i = 0; i < length; i++) {
+	    for (int j = 0; j < width; j++) {
+	        if (i<mask_size || j<mask_size || i>length-mask_size || j>width-mask_size) {
+	            udem1[i][j]=0;
+            }   
+        }
+    }
+    
+    fmatrix_move(length, width, udem1, udem1_r);
     fmatrix_zero(length, width, udem1_i);
+    
     FFTDD(udem1_r, udem1_i, length, width);
 
     float** udem1_mod = fmatrix_allocate_2d(length, width);
@@ -54,12 +76,11 @@ int main(int argc,char **argv)
     // On calcule le module de l'image 1 (F)
     Mod(udem1_mod, udem1_r, udem1_i, length, width);
 
-    float** udem2 = LoadImagePgm(NAME_IMG_IN1, &length, &width);
-    
     float best_epsilon = 1e100;
     float best_theta = -1;
 
     for (float theta = -PI / 16.0;theta < PI / 16;theta += 0.005) {
+
         float** udem2_r = fmatrix_allocate_2d(length, width);
         fmatrix_zero(length, width, udem2_r);
         fmatrix_move(length, width, udem2, udem2_r);
@@ -71,8 +92,17 @@ int main(int argc,char **argv)
         fmatrix_zero(length, width, udem2_i_rot);
         rotation(length, width, theta, udem2_r, udem2_r_rot);
 
-        FFTDD(udem2_r_rot, udem2_i_rot, length, width);
+        
+        // cadre noir sur l'image tournée
+        for (int i = 0; i < length; i++) {
+	    for (int j = 0; j < width; j++) {
+	        if (i<mask_size || j<mask_size || i>length-mask_size || j>width-mask_size) {
+	            udem2_r_rot[i][j]=0;
+            }   
+        }
+        }
 
+        FFTDD(udem2_r_rot, udem2_i_rot, length, width);
 
         float** udem2_mod = fmatrix_allocate_2d(length, width);
         fmatrix_zero(length, width, udem2_mod);
@@ -81,10 +111,9 @@ int main(int argc,char **argv)
 
         double epsilon = 0.0;
         for (int i = 0;i < length;i++) {
-            for (int j = 0;j < width;j++) {
+            for (int j =0;j < width;j++) {
                 double diff = fabs(udem2_mod[i][j] - udem1_mod[i][j]);
                 epsilon += diff;
-
             }
 
         }
--- Tp2_IFT3205_4.c
+++ Tp2_IFT3205_4.c
@@ -23,7 +23,8 @@
 #define NAME_VISUALISER "display "
 #define NAME_IMG_IN1  "UdM_1"
 #define NAME_IMG_IN2  "UdM_2"
-#define NAME_IMG_OUT1 "image-4"
+#define NAME_IMG_OUT1 "image-Out1"
+#define NAME_IMG_OUT2 "image-Out2"
 
 /*------------------------------------------------*/
 /* PROTOTYPE DE FONCTIONS  -----------------------*/
@@ -36,29 +37,50 @@ int main(int argc,char **argv)
 {
     int length,width;
     // On doit convertir en radians
-    // float Theta0 = 22.5 / 360.0 * (2.0 * PI);
+    float Theta0 = 22.5 / 360.0 * (2.0 * PI);
     char BufSystVisuImg[100];
 
     //Constante
     length=512;
     width=512;
+    int mask_size= 50;
 
-    float** udem1_r = LoadImagePgm(NAME_IMG_IN1, &length, &width);
+    float** udem1 = LoadImagePgm(NAME_IMG_IN1, &length, &width);
+    float** udem1_backup = fmatrix_allocate_2d(length, width);
+    float** udem1_r = fmatrix_allocate_2d(length, width);
     float** udem1_i = fmatrix_allocate_2d(length, width);
+    fmatrix_move(length, width, udem1, udem1_backup);
+
+    float** udem2 = LoadImagePgm(NAME_IMG_IN2, &length, &width);
+    float** udem2_backup = fmatrix_allocate_2d(length, width);
+    fmatrix_move(length, width, udem2, udem2_backup);
+
+    // les rebords noirs de l'image tournée cause des difficultés lorsqu'on
+    // trouve best_theta, on ajoute donc un cadre noir pour rendre les rebords
+    // des deux images pareilles
+    for (int i = 0; i < length; i++) {
+	    for (int j = 0; j < width; j++) {
+	        if (i<mask_size || j<mask_size || i>length-mask_size || j>width-mask_size) {
+	            udem1[i][j]=0;
+            }   
+        }
+    }
+    
+    fmatrix_move(length, width, udem1, udem1_r);
     fmatrix_zero(length, width, udem1_i);
+    
     FFTDD(udem1_r, udem1_i, length, width);
 
     float** udem1_mod = fmatrix_allocate_2d(length, width);
     fmatrix_zero(length, width, udem1_mod);
     // On calcule le module de l'image 1 (F)
-    Mod(udem1_mod, udem1_r, udem1_i,length, width);
+    Mod(udem1_mod, udem1_r, udem1_i, length, width);
 
-    float** udem2 = LoadImagePgm(NAME_IMG_IN2, &length, &width);
-    
     float best_epsilon = 1e100;
     float best_theta = -1;
 
-    for (float theta = -PI / 16.0;theta < PI / 16;theta += 0.002) {
+    for (float theta = -PI / 16.0;theta < PI / 16;theta += 0.005) {
+
         float** udem2_r = fmatrix_allocate_2d(length, width);
         fmatrix_zero(length, width, udem2_r);
         fmatrix_move(length, width, udem2, udem2_r);
@@ -70,8 +92,16 @@ int main(int argc,char **argv)
         fmatrix_zero(length, width, udem2_i_rot);
         rotation(length, width, theta, udem2_r, udem2_r_rot);
 
-        FFTDD(udem2_r_rot, udem2_i_rot, length, width);
+        // cadre noir sur l'image tournée
+        for (int i = 0; i < length; i++) {
+	    for (int j = 0; j < width; j++) {
+	        if (i<mask_size || j<mask_size || i>length-mask_size || j>width-mask_size) {
+	            udem2_r_rot[i][j]=0;
+            }   
+        }
+        }
 
+        FFTDD(udem2_r_rot, udem2_i_rot, length, width);
 
         float** udem2_mod = fmatrix_allocate_2d(length, width);
         fmatrix_zero(length, width, udem2_mod);
@@ -80,10 +110,9 @@ int main(int argc,char **argv)
 
         double epsilon = 0.0;
         for (int i = 0;i < length;i++) {
-            for (int j = 0;j < width;j++) {
+            for (int j =0;j < width;j++) {
                 double diff = fabs(udem2_mod[i][j] - udem1_mod[i][j]);
                 epsilon += diff;
-
             }
 
         }
@@ -94,28 +123,25 @@ int main(int argc,char **argv)
         }
     }
 
-    
-    float** udem2_rot = fmatrix_allocate_2d(length, width);
-    fmatrix_zero(length, width, udem2_rot);
-
-    rotation(length, width, best_theta, udem2, udem2_rot);
-
     printf("best theta: %f\n", best_theta);
+
+    float** udem2_output = fmatrix_allocate_2d(length, width);
+    rotation(length, width, best_theta, udem2_backup, udem2_output);
 
     /* free(MatriceImg1); */
     /* MatriceImg1 = MatriceImgM1; */
 
     /* //Sauvegarde */
-    SaveImagePgm(NAME_IMG_OUT1, udem2_rot,length, width);
-    // SaveImagePgm(NAME_IMG_OUT2,MatriceImg2,length,width);
+     SaveImagePgm(NAME_IMG_OUT1,udem1_backup,length, width);
+     SaveImagePgm(NAME_IMG_OUT2,udem2_output,length,width);
 
-    //Commande systeme: VISU
-    strcpy(BufSystVisuImg,NAME_VISUALISER);
-    strcat(BufSystVisuImg,NAME_IMG_OUT1);
-    strcat(BufSystVisuImg,".pgm&");
-    printf(" %s",BufSystVisuImg);
-    system(BufSystVisuImg);
-    // strcpy(BufSystVisuImg,NAME_VISUALISER);
+    /* //Commande systeme: VISU */
+    /* strcpy(BufSystVisuImg,NAME_VISUALISER); */
+    /* strcat(BufSystVisuImg,NAME_IMG_OUT1); */
+    /* strcat(BufSystVisuImg,".pgm&"); */
+    /* printf(" %s",BufSystVisuImg); */
+    /* system(BufSystVisuImg); */
+    /* // strcpy(BufSystVisuImg,NAME_VISUALISER); */
     
     /* // strcat(BufSystVisuImg,NAME_IMG_OUT2); */
     /* // strcat(BufSystVisuImg,".pgm&"); */
